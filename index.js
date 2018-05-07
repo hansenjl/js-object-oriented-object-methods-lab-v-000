4169d3cfc41ae9d5ec23a44db6f6b457eb22615b
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-05-07 20:06:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function BoardMember (name, homeState, training){
   this.name = name;
   this.homeState = homeState;
+  this.training = training; 
   this.sayHello = function(){
     console.log("Hello everybody!");
   }
