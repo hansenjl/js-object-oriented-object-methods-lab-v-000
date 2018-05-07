fe0babae59c3137d3da233fbe771efd076b838e0
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-05-07 20:12:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ function BoardMember (name, homeState, training){
   this.veto = function(){
     return "No, I must disagree";
   }
+  this.approve = function(){
+    return "You can do that!";
+  }
+  this.doCharity = function(){
+    return "I like to help people.";
+  }
 }
